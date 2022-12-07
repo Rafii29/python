74c5547decfb996a70d8f42f25cd302ffa9bf83d
Rafii29 <120013234+Rafii29@users.noreply.github.com>
2022-12-07 15:39:33 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,14 @@
+a=int(input("Enter number"))
+b=int(input("Enter number"))
+
+#Addition
+print("Addition is ",a+b)
+
+#Substraction
+print("Substraction is ",a-b)
+
+#Multiplication
+print("Multiplication is ",a*b)
+
+#Division
+print("Division is",a/b)
